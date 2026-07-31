a8df958d00ca405f662dc6ec7feb0c6534bdf73c
Jovas Motion <jovas.motion@design.com>
2026-07-31 17:04:44 -0600

    Update base path to /jovas-portfolio/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/jovas-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
